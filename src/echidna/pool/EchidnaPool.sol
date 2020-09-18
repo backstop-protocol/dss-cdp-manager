b3295b772d273b8c24e5b5474dea7d44f99bbcc6
Jitendra Chittoda <jcchittoda@gmail.com>
2020-09-18 13:39:24 +0530

    Add an invariant for Pool

--- src/echidna/pool/EchidnaPool.sol
+++ src/echidna/pool/EchidnaPool.sol
@@ -6,13 +6,17 @@ contract EchidnaPool is Pool {
 
     constructor() 
         public
-        Pool(address(0), address(0), address(0), address(0))
+        Pool(address(0), address(0), address(0), address(0), address(0))
     {
-
+        //this.setProfitParams(10, 100);
     }
 
     function echidna_test() public returns (bool) {
         return true;
+    }
+
+    function echidna_profitParams_always_ok() public returns (bool) {
+        return (shrn == 0 && shrd == 0) || shrn < shrd;
     }
 
 }
