7f6d447ffbba24d5bd42ed6650cc3c00d195d46b
Jitendra Chittoda <jcchittoda@gmail.com>
2020-09-25 19:07:51 +0530

    add logic in JarConnector to get the global and user score

--- src/JarConnector.sol
+++ src/JarConnector.sol
@@ -71,25 +71,68 @@ contract JarConnector {
         if(round == 0) return 0;
 
         uint cdp = uint(user);
-        if(round == 1) return 2 * score.getArtScore(cdp, ilk, now, start[0]);
+        if(round == 1) return _getFirstRoundUserScore(cdp, now);
 
-        uint firstRoundScore = 2 * score.getArtScore(cdp, ilk, start[1], start[0]);
+        uint firstRoundScore = _getFirstRoundUserScore(cdp, start[1]);
         uint time = now;
         if(round > 2) time = end[1];
 
         return score.getArtScore(cdp, ilk, time, start[1]) + firstRoundScore;
     }
 
+    function _getFirstRoundUserScore(uint cdp, uint endTime) internal view returns (uint) {
+        return 2 * score.getArtScore(cdp, ilk, endTime, start[0]);
+    }
+
+    /**
+     * @dev Gets user score from given endTime.
+     * @notice Function is used in Governance, to avoid score fluctualtion during voting.
+     *         The score is valid only from second round onwards.
+     * @param endTime End time of the score
+     * @return User's score from endTime to start[0]
+     */
+    function getUserScore(uint cdp, uint endTime) public view returns (uint) {
+        require(round > 2, "governance-period-not-started");
+        return score.getArtScore(cdp, ilk, endTime, start[1]) + _getFirstRoundUserScore(cdp, start[1]);
+    }
+
     function getGlobalScore() external view returns (uint) {
         if(round == 0) return 0;
 
-        if(round == 1) return 2 * score.getArtGlobalScore(ilk, now, start[0]);
+        if(round == 1) return _getFirstRoundGlobalScore(now);
 
-        uint firstRoundScore = 2 * score.getArtGlobalScore(ilk, start[1], start[0]);
+        uint firstRoundScore = _getFirstRoundGlobalScore(start[1]);
         uint time = now;
         if(round > 2) time = end[1];
 
         return score.getArtGlobalScore(ilk, time, start[1]) + firstRoundScore;
+    }
+
+    function _getFirstRoundGlobalScore(uint endTime) internal view returns (uint) {
+        return 2 * score.getArtGlobalScore(ilk, endTime, start[0]);
+    }
+
+    /**
+     * @dev Gets global score from given endTime.
+     * @notice Function is used in Governance, to avoid score fluctualtion during voting.
+     *         The score is valid only from second round onwards.
+     * @param endTime End time of the score
+     * @return Global score from endTime to start[0]
+     */
+    function getGlobalScore(uint endTime) external view returns (uint) {
+        require(round > 2, "governance-period-not-started");
+        return score.getArtGlobalScore(ilk, endTime, start[1]) + _getFirstRoundGlobalScore(start[1]);
+    }
+
+    function getPriorVotes(address account, uint blockNumber, uint proposalTime) external view returns (uint96) {
+        blockNumber; // shh
+        
+        // Get score from `proposalTime` to `start[0]`
+        // TODO get cdp from account
+        uint cdp;
+        //TODO
+        getUserScore(cdp, proposalTime);
+        return 0;
     }
 
     function toUser(bytes32 user) external view returns (address) {
